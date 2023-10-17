e639fb57f17054df1a7e11fc4c0771ba55f1a7dd
Daniel Bueno <danielbuenosan@gmail.com>
2023-10-16 19:49:43 -0600

    Fixed email issues

--- helpers/signUpEmail.js
+++ helpers/signUpEmail.js
@@ -11,36 +11,34 @@ const signUpEmail = async (data) => {
           pass: process.env.EMAIL_PASS
         },
         tls: {
-          rejectUnauthorized: false
-        }
-      });
-
-      transporter.verify(function (error, success) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log("Server is ready to take our messages");
+          rejectUnauthorized: false,
+          ciphers:'SSLv3'
         }
       });
 
     const {email, name, token} = data
 
     // Send email
-    const info = await transporter.sendMail({
-        from: 'Veterinary Patients Management',
-        to: email,
-        subject: 'Verify your account', 
-        text: 'Verify your account',
-        html: `<p>Hi ${name}!, please verify your account in VPM - Veterinary Patients Management.<p>
-            <p>Your account is ready, you only have to verify your email using this link:
-            <a href="${process.env.FRONTEND_URL}/verifyaccount/${token}">Verify Account</a> </p>
 
-            <p>If you didn't create this account, please ignore this message.</p>
-        `,
-    });
+    try {
+      const adress = process.env.EMAIL_USER
+      const info = await transporter.sendMail({
+          from: `"Veterinary Patients Management" ${adress}`,
+          to: email,
+          subject: 'Verify your account', 
+          text: 'Verify your account',
+          html: `<p>Hi ${name}!, please verify your account in VPM - Veterinary Patients Management.<p>
+              <p>Your account is ready, you only have to verify your email using this link:
+              <a href="${process.env.FRONTEND_URL}/verifyaccount/${token}">Verify Account</a> </p>
 
-    console.log('Email sent: %s', info.messageId)
-
+              <p>If you didn't create this account, please ignore this message.</p>
+          `,
+      });
+      console.log('Email sent: %s', info.messageId);
+    }
+    catch (error) {
+      console.log(error);
+    }
 
 }
 
