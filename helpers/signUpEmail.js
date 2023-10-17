142a9913890c3ed7cd73909ea447e47547790550
Daniel Bueno <danielbuenosan@gmail.com>
2023-10-16 18:47:41 -0600

    Added secure parameter to nodemailer transporter

--- helpers/signUpEmail.js
+++ helpers/signUpEmail.js
@@ -4,6 +4,7 @@ const signUpEmail = async (data) => {
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
+        secure: true,
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS
