11b6f75df7cc45c6b9729568aa22d74ce5a75aec
Daniel Bueno <danielbuenosan@gmail.com>
2023-10-16 18:59:20 -0600

    Added tls and auth type to nodemailer transporter

--- helpers/signUpEmail.js
+++ helpers/signUpEmail.js
@@ -6,8 +6,12 @@ const signUpEmail = async (data) => {
         port: process.env.EMAIL_PORT,
         secure: true,
         auth: {
+          type: 'login',
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS
+        },
+        tls: {
+          rejectUnauthorized: false
         }
       });
 
